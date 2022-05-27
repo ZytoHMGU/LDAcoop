56ad798eaa1ecddd015055d9ff1f0cd8fac6f4d2
ZytoHMGU <daniel.samaga@helmholtz-muenchen.de>
2022-05-27 10:03:29 +0200

    activity and survival calculation

--- R/LDA_survival.R
+++ R/LDA_survival.R
@@ -1,6 +1,6 @@
-#' @title LDA_activity
+#' @title LDA_survival
 #'
-#' @description calculation of activity in a table of LDA data
+#' @description calculation of clonogenic survival in a table of LDA data
 #'   (i.e. dose, number of wells, number of positive wells, group).
 #'
 #' @param x numeric data.frame or matrix with three columns (dose, number of
@@ -14,29 +14,28 @@
 #'                 "wells" = rep(25,8),
 #'                 "positive" = c(2,5,10,20,1,2,6,11),
 #'                 "group" = c(rep("A",4),rep("B",4)))
-#' act <- LDA_activity(x)
+#' act <- LDA_survival(x)
 #' @importFrom
 #' @export
-#'
-LDA_activity <- function(x,name = "LDA cells"){
+LDA_survival(x,name = "cell linea a"){
   if (!(class(x)[1] %in% c("data.frame","matrix"))){
     stop("error: x must be of class data.frame or matrix")
   }
-  if (ncol(x) == 3){
-    act <- LDA_activity_single(x,name)
+  if (ncol(x) != 4){
+    stop("error: analysis of clonogenic survival requires at least 2 groups
+         in column 4. Consider LDA_activity() for analysis of clonogenic
+         activity.")
   }
-  if (ncol(x) == 4){
-    colnames(x) <- c("dose","wells","positive","group")
-    groups <- unique(x$group)
-    act <- vector(mode = "list",length = length(groups))
-    for (i in 1:length(groups)){
-      act[[i]] <- LDA_activity_single(
-        x = subset.data.frame(x = x,
-                              subset = x$group == groups[i],
-                              select = c("dose","wells","positive"),
-                              drop = T),name,treat = groups[i])
-
-    }
+  act <- LDA_activity(x,name)
+  ref <- act[[1]]
+  sf_list <- vector(mode = "list",length = length(act)-1)
+  for (i in 1:length(sf)){
+    act.0 <- ref
+    act.1 <- act[[i+1]]
+    sf$treatment = act.1$treatment
+    sf <- LDA_survival_single(act.0 = act.0,
+                              act.x = act.1)
+    sf_list[[i]] <- sf
   }
-  return(act)
+  return(sf_list)
 }
---
+++ R/LDA_survival_single.R
@@ -0,0 +1,46 @@
+#' @title LDA_suvival_single
+#'
+#' @description calculate clonogenic survival fraction from LDA_activity objects
+#'
+#' @param act.0 reference activity
+#' @param act.x activity after treatment
+#'
+#' @return list object with survival fraction, estimated confidence intervals
+#'   (by error propagation through first order Taylor series approximation and
+#'   by combination of 84%-uncertainty-intervals of activity estimates)
+#'
+#' @examples
+#' x <- data.frame("dose" = c(10,50,100,250,10,50,100,250),
+#'                 "wells" = rep(25,8),
+#'                 "positive" = c(2,5,10,20,1,2,6,11),
+#'                 "group" = c(rep("A",4),rep("B",4)))
+#' act <- LDA_activity(x)
+#' sf <- LDA_survival(act)
+#' @importFrom
+#' @export
+LDA_survival_single <- function(act.0,act.x){
+  result <- list("treat" = act.x$treatment,
+              "sf" = NA,
+              "CI.ep" = c(NA,NA),
+              "CI.act" = c(NA,NA))
+  est.0 <- act.0$est
+  Sigma.0 <- act.0$Sigma
+  est.x <- act.x$est
+  Sigma.x <- act.x$Sigma
+
+  # g = log(sf)
+  g <- est.x[1]/est.x[2] - est.0[1]/est.0[2]
+  result$sf <- exp(g)
+
+  sig.g2 <- 1/est.0[2]^2 * ( Sigma.0[1,1] - 2 * est.0[1]/est.0[2] * Sigma.0[1,2] +
+                               (est.0[1]/est.0[2])^2 * Sigma.0[2,2] )  +
+    1/est.x[2]^2 * ( Sigma.x[1,1] - 2 * est.x[1]/est.x[2] * Sigma.x[1,2] +
+                       (est.x[1]/est.x[2])^2 * Sigma.x[2,2] )
+  sig.g <- sqrt(sig.g2)
+
+  result$CI.ep <- exp(g+qnorm(p = c(0.025,0.975))*sig.g)
+
+  result$CI.act <- c(act.0$CI.appr.SF[1]/act.x$CI.appr.SF[2],
+                     act.0$CI.appr.SF[2]/act.x$CI.appr.SF[1])
+  return(result)
+}
